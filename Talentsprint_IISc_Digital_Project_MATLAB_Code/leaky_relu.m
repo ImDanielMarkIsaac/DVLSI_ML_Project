0c5ed4c3b65e172e747ccc67b5540eef315f551e
unknown <dmivlsimlproject@gmail.com>
2024-03-25 01:32:36 +0530

    Iteration 2

--- Talentsprint_IISc_Digital_Project_MATLAB_Code/leaky_relu.m
+++ Talentsprint_IISc_Digital_Project_MATLAB_Code/leaky_relu.m
@@ -4,7 +4,8 @@ function fr = leaky_relu(x)
         if x(i)>=0
             f(i) = x(i);
         else
-            f(i) = 0.02*x(i);
+            % f(i) = 0.02*x(i);
+            f(i) = 0.001*x(i);
         end
     end
     fr = f;
