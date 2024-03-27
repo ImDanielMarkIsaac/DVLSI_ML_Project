c05358ebbd78c12b957eae7bf85af1ad7734737b
unknown <dmivlsimlproject@gmail.com>
2024-03-27 10:48:30 +0530

    Back to square one :((((

---
+++ Retrace/leaky_relu_fixed_point.m
@@ -0,0 +1,25 @@
+function fr = leaky_relu_fixed_point(x)
+    f = zeros(length(x),1);
+    % [slpfixedfloat , slpfixedinteger ,err] = fixedpoint1(0.02,35,32,1);
+    % [onefixedfloat , onefixedinteger ,err] = fixedpoint1(1,35,32,1);
+    % for i = 1:length(x)
+    %     if x(i)>=0
+    %         f(i) = onefixedinteger*x(i); % Q32 * Q32 = Q64
+    %     else
+    %         % f(i) = 0.02*x(i);
+    %         f(i) = slpfixedinteger*x(i); % Q32 * Q32 = Q64
+    %     end
+    % end
+
+    [slpfixedfloat , slpfixedinteger ,err] = fixedpoint1(0.02,11,8,1);
+    [onefixedfloat , onefixedinteger ,err] = fixedpoint1(1,11,8,1);
+    for i = 1:length(x)
+        if x(i)>=0
+            f(i) = onefixedinteger*x(i); % Q8 * Q8 = Q16
+        else
+            % f(i) = 0.02*x(i);
+            f(i) = slpfixedinteger*x(i); % Q8 * Q8 = Q16
+        end
+    end
+    fr = f;
+end
